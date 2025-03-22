1934c6ab7213cc11d8aa23c31d5fe8a8422b5f23
Dániel K <51174697+DanCodernaut@users.noreply.github.com>
2025-03-22 19:55:48 +0100

    Project setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Homepage</div>;
+};
+
+export default page;
---
+++ types/index.d.ts
@@ -0,0 +1,19 @@
+interface User {
+  name: string;
+  email: string;
+  id: string;
+}
+
+interface SignInParams {
+  email: string;
+  idToken: string;
+}
+
+interface SignUpParams {
+  uid: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+type FormType = "sign-in" | "sign-up";
