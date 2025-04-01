2eac0233ff940d0b16d8964f46f659b8b98ee108
Dániel K <51174697+DanCodernaut@users.noreply.github.com>
2025-04-01 02:42:37 +0200

    image optimization + added env (-> bug handling)

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,24 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+  },
+  images: {
+    formats: ['image/webp'],
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
   async rewrites() {
     return [
       {
-        source: "/",
-        destination: "/sign-in",
+        source: '/',
+        destination: '/sign-in',
       },
     ];
   },
 };
+
+module.exports = nextConfig;
