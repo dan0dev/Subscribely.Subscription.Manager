19d3f29f0ad1c29bfdd836f29a421614eaaa5cde
Dániel K <51174697+DanCodernaut@users.noreply.github.com>
2025-04-02 00:05:02 +0200

    feat(component): Create new component AllSubscriptionsTab.tsx

---
+++ app/(root)/tabs/AllSubscriptionsTab.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { FC } from "react";
+
+const AllSubscriptionsTab: FC = () => {
+  return (
+    <div className="flex-1 overflow-x-auto ml-5">
+      <h2 className="text-2xl font-medium text-white mb-5">All Subscriptions</h2>
+      <p className="text-light-400 text-base">Nothing to see here. admin content</p>
+    </div>
+  );
+};
+
+export default AllSubscriptionsTab;
